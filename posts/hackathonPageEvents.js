795be3804c3e8a967d4a28461ead8d19c9d7db43
Andrew <gallimoreandrew04@gmail.com>
2025-02-01 14:41:03 -0800

    Fixed hackathon message

--- posts/hackathonPageEvents.js
+++ posts/hackathonPageEvents.js
@@ -1,7 +1,7 @@
 createPost(
     {
         title: 'Hackathon 2025',
-        content: `It's coming up! The <b>2025 Hackathon</b> is going to happen March 8th and 9th. This year we are doing a hackathon for good by providing projects which will benefit local non-profits and businesses. <br><br> <b>Sign up now!</b>`,
+        content: `It's coming up! The <b>2025 Hackathon</b> is going to happen March 8th and 9th. This year we are doing a hackathon for good by providing projects which will benefit local non-profits. <br><br> <b>Sign up now!</b>`,
         size: 'half',
         show: true
     },
