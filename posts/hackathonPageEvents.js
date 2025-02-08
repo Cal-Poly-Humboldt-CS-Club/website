a68b036d6c51d084b63a48275b55cdb2aa1191d6
Andrew <gallimoreandrew04@gmail.com>
2025-02-08 13:51:14 -0800

    Updated Hackathon Page for 2025

--- posts/hackathonPageEvents.js
+++ posts/hackathonPageEvents.js
@@ -1,9 +1,9 @@
-createPost(
-    {
-        title: 'Hackathon 2025',
-        content: `It's coming up! The <b>2025 Hackathon</b> is going to happen March 8th and 9th. This year we are doing a hackathon for good by providing projects which will benefit local non-profits. <br><br> <b>Sign up now!</b>`,
-        size: 'half',
-        show: true
-    },
-    'postbody'
-);
+// createPost(
+//     {
+//         title: 'Hackathon 2025',
+//         content: `It's coming up! The <b>2025 Hackathon</b> is going to happen March 8th and 9th. This year we are doing a hackathon for good by providing projects which will benefit local non-profits. <br><br> <b>Sign up now!</b>`,
+//         size: 'half',
+//         show: true
+//     },
+//     'postbody'
+// );
